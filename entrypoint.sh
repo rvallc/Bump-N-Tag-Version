fb4fb770d642042081dc75b293eebcfa71245b6a
Michael Tesch <mtesch@utilimarc.com>
2020-04-08 14:54:34 -0500

    check the commit messages back to the last tagged version for #bump spec

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
-set -e
 
+#
+# Bashy stuff for catching errors
+#
+SCRIPT=$(basename "$0")
+# SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# ERRCOUNT=0
+onerror() {
+    echo "$(tput setaf 1)$SCRIPT: Error on line ${BASH_LINENO[0]}, exiting.$(tput sgr0)"
+    exit 1
+}
+trap onerror ERR
+
+#
+# Command line args
+#
 VERSION_FILE=$1
 tag_version=$2
 
+#
+# Environment variables
+#
 : "${PREFIX=v}"
 : "${DEFAULT_BUMP=patch}"
 : "${BUMP_FILES=}"
 : "${DRYRUN=false}"
 
+#
+# Status
+#
 echo
 echo "Input file name: $VERSION_FILE"
 echo "Tag version? ${tag_version}"
 echo "Bump files: ${BUMP_FILES}"
 echo "PREFIX: '${PREFIX}'"
-
+echo
 echo "Git Head Ref: ${GITHUB_HEAD_REF}"
 echo "Git Base Ref: ${GITHUB_BASE_REF}"
 echo "Git Event Name: ${GITHUB_EVENT_NAME}"
 echo
 echo "Starting Git Operations"
+echo
+
+# who this commit will be done as
 git config --global user.email "UtilimarcBot@utilimarc.com"
 git config --global user.name "Utilimarc GitActionBot"
 
+# https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables
 github_ref=""
 
 if test "${GITHUB_EVENT_NAME}" = "push"
@@ -42,14 +66,14 @@ else
 fi
 
 echo "File Content: $content"
-extract_string=$(echo "$content" | awk '/^([[:space:]])*(v|ver|version|V|VER|VERSION)?([[:blank:]])*([0-9]{1,2})\.([0-9]{1,3})\.([0-9]{1,4})(\.([0-9]{1,5}))?[[:space:]]*$/{print $0}')
+extract_string=$(echo "$content" | awk '/^([[:space:]])*(v|ver|version|V|VER|VERSION)?([[:blank:]])*([0-9]{1,2})\.([0-9]{1,3})\.([0-9]{1,4})[[:space:]]*$/{print $0}')
 echo "Extracted string: $extract_string"
 
 if [[ "$extract_string" == "" ]]; then
     #
     # look for the first string like "1.2.3"
     #
-    extract_string=$(echo "$content" | awk 'match($0,/"(([0-9]{1,2})\.([0-9]{1,3})\.([0-9]{1,4})(\.([0-9]{1,5}))?)"/){print substr($0,RSTART+1,RLENGTH-2); exit}')
+    extract_string=$(echo "$content" | awk 'match($0,/"(([0-9]{1,2})\.([0-9]{1,3})\.([0-9]{1,4}))"/){print substr($0,RSTART+1,RLENGTH-2); exit}')
     echo "Extracted string: $extract_string"
 fi
 
@@ -61,13 +85,33 @@ else
     echo "Valid version string found"
 fi
 
+# fetch tags
+git fetch --tags
+
+# get latest tag that looks like a semver (with PREFIX)
+tag=$(git for-each-ref --sort=-v:refname --count=1 --format '%(refname)' "refs/tags/${PREFIX}[0-9]*.[0-9]*.[0-9]*" | cut -d / -f 3-)
+tag_commit=$(git rev-list -n 1 "$tag")
+
+# get current commit hash for tag
+commit=$(git rev-parse HEAD)
+
+if [ "$tag_commit" == "$commit" ]; then
+    echo "No new commits since previous tag. Skipping..."
+    echo ::set-output "name=tag::$tag"
+    exit 0
+fi
+
+if [ -z "$tag" ]; then
+    log=$(git log --pretty='%B')
+else
+    log=$(git log "$tag..HEAD" --pretty='%B')
+fi
+
 #
-# TODO: check if commit message in `git log -n 1` contains any of
-# #major #minor #patch, if so, use it for DEFAULT_BUMP
+# determine home to bump the version supports #major, #minor, #patch
+# (anything else will be 'minor')
 #
-# get commit logs and determine home to bump the version
-# supports #major, #minor, #patch (anything else will be 'minor')
-case "$(git log -n 1)" in
+case "$log" in
     *#major*) DEFAULT_BUMP="major" ;;
     *#minor*) DEFAULT_BUMP="minor" ;;
     *#patch*) DEFAULT_BUMP="patch" ;;
@@ -83,7 +127,7 @@ newver=$(semver bump "${DEFAULT_BUMP}" "$extract_string")
 echo "Old Ver: $oldver"
 echo "Updated version: $newver" 
 
-if [ "$BUMP_FILES" = "**" ] ; then
+if [ "$BUMP_FILES" = "**" ]; then
     #
     # replace version patterns in all text files following a line containing [bump if $PREFIX]
     #
